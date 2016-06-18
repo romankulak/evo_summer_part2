26e105e46db0afdee1f30d14b5137924fcdc4b21
Roman Kulak <rome1407@gmail.com>
2016-06-18 08:11:23 +0300

    Second commit before deploy

--- talk_project/settings.py
+++ talk_project/settings.py
@@ -124,7 +124,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(REPOSITORY_ROOT, 'static/')
+STATIC_ROOT = os.path.join(REPOSITORY_ROOT, '/static/')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
